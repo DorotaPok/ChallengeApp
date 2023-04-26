136a981da118a8beee057203b4dfb4592e0158bf
dorota <pokusa_1@wp.pl>
2023-04-26 22:03:20 +0200

    Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+int age = 29;
+bool woman = true;
+
+
+if (woman )
+{
+   if(name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("EWA, LAT 33");
+    }
+    else if (age<30)
+    {
+        Console.WriteLine("KOBIETA PONIŻEJ 30 LAT");
+    }
+}
+else
+{
+    Console.WriteLine("NIEPEŁNOLETNI MĘŻCZYZNA");
+}
+
